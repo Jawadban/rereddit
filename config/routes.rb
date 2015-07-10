ef7550aeddb3f67452a8791ccf3e76b7b9488470
Jawad Bangash <Jawadban@hotmail.com>
2015-07-09 23:51:47 -0400

    still throwing some errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  resources :links
-  resources :links
+  
+resources :comments
+  
+      devise_for :users
+  resources :links do
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+       root to: "links#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
